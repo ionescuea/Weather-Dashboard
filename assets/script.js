12414bfcd563c843e03da2e8e502834260763268
ionescuea <ionescuea@yahoo.com>
2024-01-30 21:43:49 +0000

    Add comments and clean up code

--- assets/script.js
+++ assets/script.js
@@ -26,12 +26,13 @@ Object.keys(localStorage).forEach(function(key){
 });
 
 
-
+// Get city
 function getCity () {
   var longLatURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}`;
   getData(longLatURL)
 }
 
+// Get Data from city
 function getData(longLatURL) {
   console.log(longLatURL)
 
@@ -49,115 +50,90 @@ function getData(longLatURL) {
 
       var city = data.city.name;
       $("#city").text(data.city.name);
-      console.log(city);260504
   
       // Date One
       var date = data.list[0].dt_txt;
       date = dayjs(data.list[0].dt_txt).format("D/MM/YYYY");
       $("#dateOne").text(date);
-      console.log(date)
       var clouds = data.list[0].weather[0].icon;
       var iconurl = "http://openweathermap.org/img/w/" + clouds + ".png";
       $("#iconOne").attr('src', iconurl);
       var temp = (data.list[0].main.temp - 273.15).toFixed(2);
       $("#tempOne").text(temp);
-      console.log(temp);
       var wind = data.list[0].wind.speed;
       $("#windOne").text(data.list[0].wind.speed);
-      console.log(wind);
       var humidity = data.list[0].main.humidity;
       $("#humidityOne").text(data.list[0].main.humidity);
-      console.log(humidity);
   
       // Date Two
       var date = data.list[7].dt_txt;
       date = dayjs(data.list[7].dt_txt).format("D/MM/YYYY");
       $("#dateTwo").text(date);
-      console.log(date)
       var clouds = data.list[7].weather[0].icon;
       var iconurl = "http://openweathermap.org/img/w/" + clouds + ".png";
       $("#iconTwo").attr('src', iconurl);
       var temp = (data.list[7].main.temp - 273.15).toFixed(2);
       $("#tempTwo").text(temp);
-      console.log(temp);
       var wind = data.list[7].wind.speed;
       $("#windTwo").text(data.list[7].wind.speed);
-      console.log(wind);
       var humidity = data.list[7].main.humidity;
       $("#humidityTwo").text(data.list[7].main.humidity);
-      console.log(humidity);
   
       // Date Three
       var date = data.list[15].dt_txt;
       date = dayjs(data.list[15].dt_txt).format("D/MM/YYYY");
       $("#dateThree").text(date);
-      console.log(date)
       var clouds = data.list[15].weather[0].icon;
       var iconurl = "http://openweathermap.org/img/w/" + clouds + ".png";
       $("#iconThree").attr('src', iconurl);
       var temp = (data.list[15].main.temp - 273.15).toFixed(2);
       $("#tempThree").text(temp);
-      console.log(temp);
       var wind = data.list[15].wind.speed;
       $("#windThree").text(data.list[15].wind.speed);
-      console.log(wind);
       var humidity = data.list[15].main.humidity;
       $("#humidityThree").text(data.list[15].main.humidity);
-      console.log(humidity);
   
       // Date Four    
       var date = data.list[23].dt_txt;
       date = dayjs(data.list[23].dt_txt).format("D/MM/YYYY");
       $("#dateFour").text(date);
-      console.log(date)
       var clouds = data.list[23].weather[0].icon;
       var iconurl = "http://openweathermap.org/img/w/" + clouds + ".png";
       $("#iconFour").attr('src', iconurl);
       var temp = (data.list[23].main.temp - 273.15).toFixed(2);
       $("#tempFour").text(temp);
-      console.log(temp);
       var wind = data.list[23].wind.speed;
       $("#windFour").text(data.list[23].wind.speed);
-      console.log(wind);
       var humidity = data.list[23].main.humidity;
       $("#humidityFour").text(data.list[23].main.humidity);
-      console.log(humidity);
   
       // Date Five
       var date = data.list[31].dt_txt;
       date = dayjs(data.list[31].dt_txt).format("D/MM/YYYY");
       $("#dateFive").text(date);
-      console.log(date)
       var clouds = data.list[31].weather[0].icon;
       var iconurl = "http://openweathermap.org/img/w/" + clouds + ".png";
       $("#iconFive").attr('src', iconurl);
       var temp = (data.list[31].main.temp - 273.15).toFixed(2);
       $("#tempFive").text(temp);
-      console.log(temp);
       var wind = data.list[31].wind.speed;
       $("#windFive").text(data.list[31].wind.speed);
-      console.log(wind);
       var humidity = data.list[31].main.humidity;
       $("#humidityFive").text(data.list[31].main.humidity);
-      console.log(humidity);
       
       // Date Six            
       var date = data.list[39].dt_txt;
       date = dayjs(data.list[39].dt_txt).format("D/MM/YYYY");
       $("#dateSix").text(date);
-      console.log(date)
       var clouds = data.list[39].weather[0].icon;
       var iconurl = "http://openweathermap.org/img/w/" + clouds + ".png";
       $("#iconSix").attr('src', iconurl);
       var temp = (data.list[39].main.temp - 273.15).toFixed(2);
       $("#tempSix").text(temp);
-      console.log(temp);
       var wind = data.list[39].wind.speed;
       $("#windSix").text(data.list[39].wind.speed);
-      console.log(wind);
       var humidity = data.list[39].main.humidity;
       $("#humiditySix").text(data.list[39].main.humidity);
-      console.log(humidity);
     });
 }
 
@@ -180,4 +156,3 @@ function updateData() {
 }
 
 searchButton.addEventListener("click", updateData);
-
